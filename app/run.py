dcf36ea067078efe589b0e26a9afd30498f59e81
anthonystem <anthony_stem@outlook.com>
2023-12-02 01:36:39 -0500

    Added route for login and basic layout for content sections.

--- app/run.py
+++ app/run.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", title="PyMart Intranet System | Welcome", page="landing")
+
+@app.route("/login")
+def login():
+    return render_template("login.html", title="PyMart Intranet System | Login", page="login")
 
 if __name__ == "__main__":
     app.run(debug=True)
