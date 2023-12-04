24f3cf6160ef0a422e1d7f7cb2505c139cdfa3bc
anthonystem <anthony_stem@outlook.com>
2023-12-04 16:39:14 -0500

    Implemented validation functions for password and username.
    Will likely move these functions out of app.py later.

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, redirect, request, session, url_for
+from string import ascii_lowercase, ascii_uppercase
 import hashlib
 import os
 
@@ -41,15 +42,11 @@ def dashboard():
 def hash_password(password: str) -> str:
     # Generate random salt that is 40 characters long.
     salt = os.urandom(20).hex()
-    print("Salt:", salt)
 
     to_hash = salt + password
-    print("Salt + PW:", to_hash)
     to_hash = to_hash.encode("utf-8")
-    print("Encoded S+P:", to_hash)
 
     password_hash = hashlib.sha256(to_hash).hexdigest()
-    print("HASH:", password_hash)
 
     # Prepend salt to password_hash for storage.
     salt_and_password_hash = salt + password_hash
@@ -70,6 +67,67 @@ def authenticate(plaintext_password: str, stored: str) -> bool:
     attempt_hash = hashlib.sha256(to_hash).hexdigest()
 
     return attempt_hash == password_hash
+
+
+def validate_username(username: str) -> bool:
+    """Verifies if username meets username requirements.
+    The username requirements are as follows:
+    - Minimum length is 3.
+    - Maximum length is 16.
+
+    Args:
+        username (str): The username string to validate.
+
+    Returns:
+        bool: True if username meets requirements; otherwise, False.
+    """
+    if len(username) < 3 or len(username) > 16:
+        return False
+    
+    return True
+
+
+def validate_password(password: str) -> bool:
+    """Checks that the password meets all the password requirements.
+    These requirements include the following:
+    - Minimum length is 8.
+    - Maximum length is 25.
+    - Has at least one number.
+    - Has at least one lowercase letter.
+    - Has at least one uppercase letter.
+    - Has at least one special character (" !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~").
+
+    Args:
+        password (str): The password string being validated.
+
+    Returns:
+        bool: True if the password meets the requirements; otherwise, False.
+    """
+    special_characters = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+
+    # Verify length requirement.
+    if len(password) < 8 or len(password) > 25:
+        return False
+    
+    # Verify character requirements.
+    has_number = False
+    has_lower = False
+    has_upper = False
+    has_special = False
+    for char in password:
+        if char.isdigit():
+            has_number = True
+        elif char in ascii_lowercase:
+            has_lower = True
+        elif char in ascii_uppercase:
+            has_upper = True
+        elif char in special_characters:
+            has_special = True
+
+    if not has_number or not has_lower or not has_upper or not has_special:
+        return False
+    
+    return True
 
 if __name__ == "__main__":
     app.run(debug=True)
