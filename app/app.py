ac5cf983bea5d5975a3e18cc93800979abab2cb5
anthonystem <anthony_stem@outlook.com>
2023-12-06 23:14:24 -0500

    Added sessions.
    Session is created when user logs in.

--- app/app.py
+++ app/app.py
@@ -6,47 +6,67 @@ import config
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object("config")
+app.secret_key = app.config['SECRET_KEY']
 
 @app.route("/")
 def index():
+    # Render dashboard if session is already set.
+    if session.get("username", None) is not None:
+        return redirect("/dashboard")
+    
     print(app.config["SECRET_KEY"])
     return render_template("index.html", title="PyMart Intranet System | Welcome", page="landing")
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    # Render dashboard if session is already set.
+    if session.get("username", None) is not None:
+        return redirect("/dashboard")
+    
     errors = []
     if request.method == "POST":
         ### Get login form data.
         username = request.form.get("username")
         password = request.form.get("password")
-        print("Username:", username)
-        print("Password:", password)
 
         ### Check if input credentials are in the database.
-        Database.setup(test_mode=True)
         connection = sqlite3.connect(config.DATABASE_FILE)
         cursor = connection.cursor()
+
         cursor.execute("SELECT password FROM Users WHERE username = ?", (username,))
         stored_password = cursor.fetchone()
+
         if stored_password is None:
-            print("Could not find username and password.")
             valid_credentials = False
         else:   
             valid_credentials = authentication.authenticate(password, stored_password[0])
 
         ### Handle valid and invalid credentials.
-        print("Valid Credentials?", valid_credentials)
 
-        # Redirect user to dashboard if correct login data; otherwise, append error and render login.
+        # Redirect user to dashboard and initialize session if correct login data.
+        # Otherwise, append incorrect username/password error and re-render login.
         if valid_credentials:
+            # Get user_id and create session with user_id and username; redirect to dashboard after.
+            cursor.execute("SELECT user_id FROM Users WHERE username = ?", (username,))
+            user_id = cursor.fetchone()[0]
+            session["user_id"] = user_id
+            session["username"] = username
+            cursor.close()
+            connection.close()
             return redirect(url_for("dashboard"))
         
+        cursor.close()
+        connection.close()
+
         errors.append("Incorrect username or password. Please try again.")
+
+
 
     return render_template("login.html", title="PyMart Intranet System | Login", page="login", form_errors=errors)
 
 @app.route("/dashboard")
 def dashboard():
+    print(session)
     return render_template("dashboard.html", title="PyMart Intranet System | Dashboard", page="dashboard")
 
 if __name__ == "__main__":
